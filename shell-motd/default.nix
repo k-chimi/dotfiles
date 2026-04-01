e34b9ff5c1279ce4e384ddc2f93ce28afb8d7651
KChimi <260343942+k-chimi@users.noreply.github.com>
2026-04-01 10:11:13 +0000

    現在のシェル環境についての Motd を表示するように

---
+++ shell-motd/default.nix
@@ -0,0 +1,109 @@
+{ writeText }:
+  let
+    ansi-colors = import ../lib/ansi-colors.nix { mode = "sh"; };
+    fg = ansi-colors.fg;
+    bg = ansi-colors.bg;
+  in  
+    writeText "shell-motd.sh" ''
+      # [ ! -z "$__SHELL_MOTD" ] && return
+      # export __SHELL_MOTD=1
+      
+      {
+        print-title() {
+          echo -e "$1$2"
+        }
+        
+        print-entry() {
+          echo -e "$1$(printf ''\'%s: ${fg.ansi256 "223"}%s${fg.reset}''\' "$2" "$3")"
+        }
+        
+        print-entry-block() {
+          echo -e "$1$2:"
+
+          (
+            i=0
+            more_count=0
+            IFS=$' '
+
+            echo -n "$3"
+
+            for x in $4; do
+              if [ "$i" -lt 15 ]; then
+                if [[ "$x" =~ $'\n' ]]; then
+                  sep=$'\n'"$3"
+                elif [ "$i" -eq 0 ]; then
+                  sep=
+                else
+                  sep=" "
+                fi
+                
+                echo -n "$sep$(printf ''\'${fg.ansi256 "223"}%s${fg.reset}''\' "''${x#$'\n'}")"
+                (( i ++ ))
+              else
+                (( more_count ++ ))
+              fi
+            done
+
+            [ "$sep" == " " ] && echo
+
+            if [ "$more_count" -gt 0 ]; then
+              echo "$3$(printf ''\'${fg.ansi256 "245"}( more %s )${fg.reset}''\' "$more_count")"
+            fi
+          )
+        }
+
+        get-raw-nix-packages() {
+          echo $PATH \
+            | tr ':' '\n' \
+            | grep -Eo "$NIX_STORE/[a-z0-9]+(-[^-/]+)+" \
+            | sed -nE 's/\/([^/]+\/)+[a-z0-9]+-([^-/]+(-[^-/0-9]+)*).*$/\2/gp'
+        }
+
+        get-nix-packages() {
+          (
+            IFS=$'\n'
+            local -A entries=()
+
+            for x in $(get-raw-nix-packages); do
+              if [ -z "''${entries[$x]}" ]; then
+                echo $x
+                entries[$x]=1
+              fi
+            done
+          )
+        }
+
+        print-nix-packages() {
+          (
+            IFS=$'\n'
+            i=1
+
+            for x in $(get-nix-packages); do
+              echo -n "$x"
+            
+              if [ ! "$i" -lt 5 ]; then
+                echo -n $' \n'
+                i=1
+              else
+                echo -n ' '
+                (( i ++ ))
+              fi
+            done
+          )
+        }
+      
+        main() {
+          local PAD="  "
+
+          echo
+          print-title "$PAD" '${ansi-colors.bold}${fg.ansi256 "111"}Nix${ansi-colors.reset}'
+          print-entry "$PAD$PAD" "Name" "$name"
+          print-entry "$PAD$PAD" "System" "$system"
+          print-entry "$PAD$PAD" "IN_NIX_SHLL" "$IN_NIX_SHELL"
+          print-entry-block "$PAD$PAD" "Packages" "$PAD$PAD$PAD" "$(print-nix-packages)"
+          echo
+        }
+
+        main $@
+      }
+    ''
