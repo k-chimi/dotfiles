f6874c03ac1f2c5f7f09a11b149c7ef5c60e4bb1
KChimi <260343942+k-chimi@users.noreply.github.com>
2026-04-01 12:56:04 +0000

    fix: shell-ps1 で重複した表示が出ないように修正

--- shell-ps1/default.nix
+++ shell-ps1/default.nix
@@ -11,10 +11,15 @@
       packages = [ which prompt-command ];
     
       shellHook = ''
-        export PROMPT_FMT=''\'''${PROMPT_STATUS@P}''\'"${ansi-colors.bold}${fg.light_green}\u@\H${ansi-colors.reset} ${ansi-colors.bold}"''\'''${PROMPT_CWD@P}''\'"${ansi-colors.reset}"''\'''${PROMPT_BRANCH@P}''${PROMPT_BRANCH_CHANGES@P}''${PROMPT_NIX_SHELL@P}\n''\'"\$ "
-        export PS1="$PROMPT_FMT"
+        if [ -z "$__SHELL_PS1_INSTALLED" ]; then
+          export __SHELL_PS1_INSTALLED=1
+          export PROMPT_FMT=''\'''${PROMPT_STATUS@P}''\'"${ansi-colors.bold}${fg.light_green}\u@\H${ansi-colors.reset} ${ansi-colors.bold}"''\'''${PROMPT_CWD@P}''\'"${ansi-colors.reset}"''\'''${PROMPT_BRANCH@P}''${PROMPT_BRANCH_CHANGES@P}''${PROMPT_NIX_SHELL@P}\n''\'"\$ "
+          export PS1="$PROMPT_FMT"
 
-        export PROMPT_COMMAND="''${PROMPT_COMMAND/%/;}. ${prompt-command}/__prompt_command \$?"
+          export PROMPT_COMMAND="''${PROMPT_COMMAND/%/;}eval \''${__SHELL_PS1}"
+        fi
+
+        export __SHELL_PS1=". ${prompt-command}/__prompt_command \$?"
         export PROMPT_COMMAND_ADDITIONAL="prompt_cwd prompt_nix prompt_git"
 
         export PROMPT_STATUS=""
